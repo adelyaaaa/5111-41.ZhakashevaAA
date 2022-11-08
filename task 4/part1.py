ec438a3024dff10081427661ce2e3a3f61718c9e
Жакашева Аделина <adelya.zha03@gmail.com>
2022-11-08 23:15:16 +0300

    лаба 4

--- task 4/part1.py
+++ task 4/part1.py
@@ -1,22 +1,22 @@
-num_list = []
-num = int(input())
+num_list=[]
+num = int(input("Введите значения:"))
 while num !=0:
    num_list.append(num)
-   num = int(input())
-for i in range (0,len(num_list)):
-   for j in range(0,len(num_list)-1):
+   num = int(input("Введите значения:"))
+for i in range(0, len(num_list)):
+   for j in range(0, len(num_list)-1):
       if num_list [j]>num_list[j+1]:
-         num_list [j], num_list[j+1]= num_list[j+1], num_list[j+1]
-num_list.sort()
-min = num_list [0]
-i = 0
+         num_list[j], num_list[j+1] = num_list[j+1], num_list[j]
+min = num_list[0]
+max = num_list[0]
+l = 0
 for value in num_list:
-   if value<min:
-      min=value
-max = num_list [0]
-for value in num_list:
-   if value>max:
-      max=value
-      i+=1
-      num_list.pop(j)
+   if value < min:
+      min = value
+   if value > max:
+      max = value
+   l += 1
+for k in range(0, len(num_list)):
+   if k % 2 == 0:
+      num_list.pop(k)
 print(num_list)
--- task 4/part2.py
+++ task 4/part2.py
@@ -1,9 +1,8 @@
 word_dict = {}
-word = input()
+word = input("Введите слова: ")
 while word:
     if not word_dict.get(word):
         word_dict.update({word:word})
-    word=input()
-print(word_dict)
-for i in word_dict.keys ():
-    print (i,end = " ")
+    word=input("Введите слова: ")
+for word in word_dict.values():
+    print(word)
