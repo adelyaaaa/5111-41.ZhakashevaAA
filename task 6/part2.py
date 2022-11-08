704aa5e9f749d12531d5f1eb7d7e69de4519d2f9
Жакашева Аделина <adelya.zha03@gmail.com>
2022-11-08 23:15:33 +0300

    лаба 6

---
+++ task 6/part2.py
@@ -0,0 +1,25 @@
+import numpy
+import math 
+first_matrix =  numpy.array([[3, 5, 9], [1, 6, 4], [3, 2, 1]])
+second_matrix = numpy.array([[10, 11, 12], [16, 17, 18], [13, 14, 15]])
+
+def are_valid(first_matrix:numpy.ndarray , second_matrix:numpy.ndarray ):
+       return (first_matrix.shape[0] == second_matrix.shape[1]) and (first_matrix.shape[1] == second_matrix.shape[0])
+
+def mat_mult(first_matrix, second_matrix ):
+    res_matrix = numpy.ndarray((first_matrix.shape [0], second_matrix.shape [1]))
+    for i in range(res_matrix.shape[0]):
+        for j in range(res_matrix.shape[1]):
+            for k in range(res_matrix.shape[0]):
+                res_matrix[i, j]+= first_matrix[i, k]*second_matrix[k, j]
+    return res_matrix
+C = first_matrix.dot(second_matrix)
+det_1 = round(numpy.linalg.det(first_matrix))
+det_2 = round(numpy.linalg.det(second_matrix))
+rank1 = numpy.linalg.matrix_rank(first_matrix)
+rank2 = numpy.linalg.matrix_rank(second_matrix)
+print(C)
+print(det_1)
+print(det_2)
+print(rank1)
+print(rank2)
